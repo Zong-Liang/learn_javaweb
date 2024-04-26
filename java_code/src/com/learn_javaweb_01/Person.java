11deb33230f74c77bbe4ff7495f357e2f7ff4abd
ZL85 <zongliang0726@163.com>
2024-04-26 11:01:07 +0800

    update

---
+++ java_code/src/com/learn_javaweb_01/Person.java
@@ -0,0 +1,63 @@
+package com.learn_javaweb_01;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+    private Dog dog;
+
+    public Person() {
+    }
+
+    public Person(String name, int age, Dog dog) {
+        this.name = name;
+        this.age = age;
+        this.dog = dog;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", dog=" + dog +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person person)) return false;
+        return age == person.age && name.equals(person.name) && dog.equals(person.dog);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, dog);
+    }
+}
